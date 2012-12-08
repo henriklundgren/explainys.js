3324e01fac1fd339b517dfbe24be1706387c8a67
Henrik Lundgren <upptaget@gmail.com>
2012-12-08 21:47:12 +0100

    change to html5 details and summary markup

--- js/jquery.explainys.js
+++ js/jquery.explainys.js
@@ -16,7 +16,9 @@
             // text to show when no explanation given
             textFail: "Nothing to show",
             // minus or plus the div position
-            divPosition: +5,
+            divPosition: 0,
+            // class name for number on div
+            divNumbers: "numbers",
 
         };
         var options = $.extend(defaults, options);
@@ -48,14 +50,10 @@
             text: (index + 1)
         }).insertBefore($this);
 
-	// Read more/less
-        var sylvie = $('<button />', {
-            class: 'sylvie',
-            text: 'show more',
-            });
+	
 
         // #1. Create DIV(s)
-        $('<div />', {
+        $('<details />', {
             class: options.divClass,
             })
             // #2. append the DIV(s) to a container
@@ -67,11 +65,12 @@
                 var $that = $(this);
                 // number on div
                 var number = $('<span />', {
-                    class: 'numbers',
+                    class: options.divNumbers,
                     text: (index + 1),
-                });
-                // append index number
-                $that.append(number);
+                    });
+                
+
+
                 // highlight origin
                 $that.on({
                     mouseenter: function() {
@@ -84,50 +83,34 @@
                     },
                     mouseleave: function() {
                         $this
-                        // remove css background
+                        // remove css background - inherit not supported by IE
                         .css('background-color', 'inherit');
                     }                    
                 });
+
+
                 // append title
                 if (typeof $datatitle !== 'undefined' && $datatitle.length > 0) {
-                    $that.append('<h6>' + $datatitle + '</h6>');
+                    $that.append('<summary>' + $datatitle + '</summary>');
                 } else {
-                    $that.append('<h6>' + $datatitle_fail + '</h6>');
+                    $that.append('<summary>' + $datatitle_fail + '</summary>');
                 }
+
+                // append ref. number
+                $($that).find('summary').append(number);
+
+                
                 // append text
                 if (typeof $datatext !== 'undefined' && $datatext.length > 0) {
                    $that.append('<p>' + $datatext + '</p>');
                 } else {
                    $that.append('<p>' + options.textFail + '</p>');
                 }
-            })// end each
+                
+                
 
-            // append truncate button div container
-            .append($('<div />', {
-                class: 'henke'
-            })
-            // append the actual html5 button
-            .append(sylvie))
-            // what happens when you push it?
-            // not working because the context is the full div
-            .on({
-                mouseenter: function() {
-                    console.log('hit');
-                    $(this)
-                    .parents().find('.explain_it > p')
-                    // add css background
-                    .css('max-height', 'auto')
-                    // animate mouseenter
-                    // (coloranimation not supported in vanilla jquery)
-                    .animate({backgroundColor: '#DFF9AE'}, 'slow');
-                    },
-                    click: function() {
-                    $(this)
-                    // remove css background
-                    .css('max-height', '30');
-                    }
+            });// end each
 
-                    });
             
 
     });// end each

3316228e474fa20ee133ea2d6ca6a5da05eeb7a0
Henrik Lundgren <upptaget@gmail.com>
2012-12-12 15:03:22 +0100

    continue collision detection

--- js/jquery.explainys.js
+++ js/jquery.explainys.js
@@ -14,7 +14,7 @@
             // where to place the div(s)?
             targetDiv: "#sidecontent",
             // text to show when no explanation given
-            textFail: "Someone forgot to write the sidenote.&#9786;",
+            textFail: "Opps!!! Someone forgot to write the sidenote.&#9786;",
             // minus or plus the div position
             divPosition: 0,
             // class name for number on div
@@ -25,63 +25,97 @@
         };
         var options = $.extend(defaults, options);
        
- 
+
+
+
+    // Create mark element
+    $('<mark />', {
+        text: 'test',
+        id: 'xyz123'
+    })
+    .appendTo('body')
+    .css({'position': 'absolute', 'left': '-9999'});
+    // Get mark element background color
+    mark = $('mark').css('background-color');
+    $('#xyz123').remove();
+    // console.log(mark);
+
+
+
+
 
     // Each time jQuery finds span... 
 	$(options.spanClass).each(function(index, value, position) {
 
 
-    // cache $this so it only jumps in the pool once
-    var $this = $(this);
-    // get the vertical position
-    var ptr = $this.position().top;
-    // get computed height of span/word
-    var $ltr = $this.height() + options.divPosition;
-    // get the explain text in data-explain
-    var $datatext = $this.data('explain');
-    // Get the title text if given
-    var $datatitle = $this.data('title');
-    // get the content of the span
-    var $datatitle_fail = $this.text();
+    // Some variables
+    var 
+        // Only jump in the pool once.
+        $this = $(this),
+        // Get the vertical position.
+        ptr = $this.position().top,
+        // Get computed height of span/word.
+        $ltr = $this.height() + options.divPosition,
+        // Get the explain text in html5 data-explain.
+        $datatext = $this.data('explain'),
+        // Get the html5 data-title text if given.
+        $datatitle = $this.data('title'),
+        // Get the content of the span.
+        $datatitle_fail = $this.text();
 
+    
+    
 
     
 
-    // Insert reference number
-        $('<sup />', {
-            text: (index + 1),
+    // Reference number before span
+    $('<sup />', {
+        text: (index + 1),
         })
-        // Dont let users select the ref number
-        .css({'webkit-user-select': 'none'})
+        
+        // Dont let users select the ref number text (non-standard)
+        .css({
+            '-webkit-user-select': 'none',
+            '-moz-user-select': 'none',
+            '-ms-user-select': 'none',
+            'user-select': 'none',
+            })
+        
         // Insert ref number before the span
         .insertBefore($this);
 
 	
 
-        // Create html5 details element
-        $('<details />', {
-            class: options.divClass,
-            id: 'special' + (index + 1),
-            })
 
-            // Options for the details default state
-            .attr(options.divOpen, options.divOpen)
-            
-            // Append details element to target 
-            .appendTo(options.targetDiv)
-            
-            // Position element
-            .css({'top': ptr -$ltr})
-            
-            // For each of the details element do this...
-            .each(function(name, i) {
-                var $that = $(this);
 
-                // Reference number
-                var number = $('<span />', {
-                    class: options.divNumbers,
-                    text: (index + 1),
-                    });
+    // Create html5 details element. (Limited support)
+    $('<details />', {
+        class: options.divClass,
+        id: 'explainy_ref' + (index + 1),
+        })
+
+        // Options for the details default state
+        .attr(options.divOpen, options.divOpen)
+            
+        // Append details element to target 
+        .appendTo(options.targetDiv)
+            
+        // Position element
+        .css({'top': ptr -$ltr})
+            
+        
+
+
+        // For each of the details element do this...
+        .each(function(name, i) {
+            
+            var $that = $(this);
+
+            // Reference number
+            var number = $('<span />', {
+                class: options.divNumbers,
+                text: (index + 1),
+                });
                
 
 
@@ -92,38 +126,41 @@
 
                     // If option divOpen set to open
                     if (options.divOpen == 'open') {
-                        $(this).css({'z-index': '999'});
-                    // Do nothing enter
+                        
+                    // Do nothing
                     // console.log('Do nothing!');
                     } else {
 
                     // Else open the detail element
-                    $(this).attr('open', 'open').css({'z-index': '999'});
+                    $(this).attr('open', 'open');
                     } // end if
+
+
 
                     $this
                     // Add color background when hovering
-                    .css({'background-color': 'pink'})
+                    .css({'background-color': mark})
+
                     
                     },
                     mouseleave: function() {
-                        // console.log('closed');
+                    // console.log('closed');
                     
-                        // If option divOpen set to open
-                        if (options.divOpen == 'open') {
+                    // If option divOpen set to open
+                    if (options.divOpen == 'open') {
 
-                        // Do nothing enter
-                        //console.log('Do nothing!');
-                        } else {
+                    // Do nothing enter
+                    //console.log('Do nothing!');
+                    } else {
 
-                        // Else open the detail element
-                        $(this).removeAttr('open');
-                        } // end if
+                    // Else open the detail element
+                    $(this).removeAttr('open');
+                    } // end if
 
 
-                        $this
-                        // Remove color background when not hovering
-                        .css('background-color', 'transparent');
+                    $this
+                    // Remove color background when not hovering
+                    .css('background-color', 'transparent');
 
                     } // end function    
 
@@ -134,33 +171,102 @@
                 if (typeof $datatitle !== 'undefined' && $datatitle.length > 0) {
                     // Attribute title
                     $that
-                    .append('<summary>' + '<span>' + $datatitle + '</span>' + '</summary>')
-                    .find('summary > span:first-child').addClass('explainTitle');
+                    .append('<summary>' + '<span>' + $datatitle + '</span>' + '</summary>');
                 } else {
                     // Content of span
-                    $that.append('<summary>' + '<span>' + $datatitle_fail + '</span>' + '</summary>')
-                    .find('summary > span:first-child').addClass('explainTitle');
+                    $that
+                    .append('<summary>' + '<span>' + $datatitle_fail + '</span>' + '</summary>');
                 }
 
-                // Append ref. number
-                $that.find('summary').append(number);
+
+                $that
+                // Append ref. number on summary
+                .find('summary')
+                .append(number)
+                .end()
+                // Add class to title span
+                .find('summary > span:first-child')
+                .addClass('explainTitle');
 
 
                 // Append text
                 if (typeof $datatext !== 'undefined' && $datatext.length > 0) {
                     // Attribute data-explain
-                    $that.append('<p>' + $datatext + '</p>');
+                    $that
+                    .append('<p>' + $datatext + '</p>');
                 } else {
                     // If non existing
-                    $that.append('<p>' + options.textFail + '</p>')
+                    $that
+                    .append('<p>' + options.textFail + '</p>')
                     .removeAttr('open');
                 }
                 
-                
-            }); // end each  
+
+                // Add link to sidenotes
+                var linky = $that.attr('id'),
+                    linky2 = $('<a />', {
+                            class: 'explainLink',
+                            text: 'Link',
+                            href: '#' + linky,
+                            });
+                var maily = $('<a />', {
+                            class: 'explainMail',
+                            text: ' Share',
+                            // Later it should facilitate some kind of sharing
+                            href: '#',
+                            });
+
+                // Append the links
+                $that.append(linky2).find('a.explainLink').wrap('<small>');
+                $that.append(maily).find('a.explainMail').wrap('<small>');
+
+
+            }); // end each for detail element 
      
 
-        });// end each
+        });// end each for span element
+
+
+
+
+// This finally works!!! :)) My detection method
+
+// Topvalue array
+var topvalue_arr = [];
+// Bottomvalue array
+var bottomvalue_arr = [];
+
+// Populate the arrays
+$('.explain_it').each (function() {
+    // Push top position to array
+    topvalue_arr.push($(this).position().top);
+    // Push bottom position (top position + element height) to array
+    bottomvalue_arr.push($(this).outerHeight() + $(this).position().top);
+});
+console.log('topvalues: ' + topvalue_arr); // works!!!
+console.log('bottomvalues: ' + bottomvalue_arr); // works!!!
+
+// React to array values
+if (bottomvalue_arr[0] > topvalue_arr[1]) {
+    console.log('do something');
+    // target the second div and move it down
+    $('.explain_it').eq(1).css({'top': bottomvalue_arr[1]+60, 'border': '3px dashed red'});
+} else {
+    console.log('do nothing');
+}
+
+// React to array values
+if (bottomvalue_arr[1] > topvalue_arr[2]) {
+    console.log('do something');
+    // target the second div and move it down
+    $('.explain_it').eq(2).css({'top': bottomvalue_arr[2]+60, 'border': '3px dashed lightblue'});
+} else {
+    console.log('do nothing');
+}
+
+
+
+
 
 };
         
